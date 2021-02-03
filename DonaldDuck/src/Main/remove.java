812aaf3e7170167f5a66aaa3125daeb21369a24c
PuaZhiXian <78425201+PuaZhiXian@users.noreply.github.com>
2021-02-03 23:31:42 +0800

    Update remove.java

--- DonaldDuck/src/Main/remove.java
+++ DonaldDuck/src/Main/remove.java
@@ -8,15 +8,15 @@ public class remove extends sort {
 
     Scanner sc = new Scanner(System.in);
 
-    public void remove(String[] element, int num) {
+    public void remove(String[] element, int num) {                  // remove double / repeated elements
         System.out.println("run remove method \n");
-        String[][] remove_after = new String[data.length - 1][];
+        String[][] remove_after = new String[data.length - 1][];       /// create a array that not have first row
         for (int i = 0; i < remove_after.length; i++) {
             remove_after[i] = data[i + 1];
         }
         int[] index = new int[element.length];
         int counter = 0;
-        for (int i = 0; i < data[0].length; i++) {
+        for (int i = 0; i < data[0].length; i++) {                // find which colomun choosed
             if (data[0][i].equals(element[counter])) {
                 index[counter] = i;
                 counter++;
@@ -29,17 +29,17 @@ public class remove extends sort {
             int start = 0;
             int end = 0;
             int anum = index[i];
-            Arrays.sort(remove_after, Comparator.comparing(a -> a[anum]));
+            Arrays.sort(remove_after, Comparator.comparing(a -> a[anum]));          //  sorting   a[anum]  mean sort based on colomun [anum] 
             for (int j = 0; j < remove_after.length - 1; j++) {
-                if (remove_after[j][anum].equals(remove_after[j + 1][anum])) {
+                if (remove_after[j][anum].equals(remove_after[j + 1][anum])) {          //  find which the sama element start and end
                     end++;
-                } else if (num > (end - start + 1)) {
+                } else if (num > (end - start + 1)) {              /// handle posibility where user want remain the number3 data but there only have 2 data 
                     for (int k = start + 1; k <= end; k++) {
                         remove_after[k][0] = " ";
                     }
                     start = end;
                 } else {
-                    for (int k = start; k <= end; k++) {
+                    for (int k = start; k <= end; k++) {         // set the first elements to empty // i assume the fisrt elements is primary key can not empty one
                         if (k != start + (num - 1)) {
                             remove_after[k][0] = " ";
                         }
@@ -49,16 +49,16 @@ public class remove extends sort {
             }
         }
 
-        Arrays.sort(remove_after, Comparator.comparing(a -> a[0]));
+        Arrays.sort(remove_after, Comparator.comparing(a -> a[0]));        //sort so empty will at infront
         int size = 0;
         for (int i = 0; i < remove_after.length; i++) {
-            if (remove_after[i][0].equals(" ")) {
+            if (remove_after[i][0].equals(" ")) {               // only can which no empty 
                 size++;
             }
         }
-        String[][] remove = new String[remove_after.length - size + 1][];
+        String[][] remove = new String[remove_after.length - size + 1][];      
         int con = 1;
-        remove[0] = data[0].clone();
+        remove[0] = data[0].clone();                             // copy the data to remove(array) with have first row one
         for (int i = 0; i < remove_after.length; i++) {
             if (i >= size) {
                 remove[con] = remove_after[i];
@@ -66,7 +66,7 @@ public class remove extends sort {
             }
         }
 
-        for (int i = 0; i < remove.length; i++) {
+        for (int i = 0; i < remove.length; i++) {           // display
             for (int j = 0; j < remove[i].length; j++) {
                 System.out.print(remove[i][j] + " ");
             }
@@ -75,10 +75,10 @@ public class remove extends sort {
 
     }
 
-    public void remove(String[] elements) {
+    public void remove(String[] elements) {           // remove empty 
         int[] index = new int[elements.length];
         int counter = 0;
-        for (int i = 0; i < elements.length; i++) {
+        for (int i = 0; i < elements.length; i++) {     // find which colomun choosed 
             for (int j = 0; j < data[0].length; j++) {
                 if (elements[i].equals(data[0][j])) {
                     index[counter] = j;
@@ -89,8 +89,8 @@ public class remove extends sort {
         }
         String[][] remove_after = data.clone();
         int size = 0;
-        for (int i = 1; i < remove_after.length; i++) {
-            int con = 0;
+        for (int i = 1; i < remove_after.length; i++) {         //  sama la just check whether condition mean then set ti to empty
+            int con = 0; 
             for (int j = 0; j < index.length; j++) {
                 if (remove_after[i][index[j]].equals(" ")) {
                     con++;
@@ -102,7 +102,7 @@ public class remove extends sort {
             }
         }
         int con = 0;
-        String[][] remove = new String[remove_after.length - size][];
+        String[][] remove = new String[remove_after.length - size][];          // create a new array with the empty one deleted
         for (int i = 0; i < remove_after.length; i++) {
             if (!remove_after[i][0].equals(" ")) {
                 remove[con] = remove_after[i];
@@ -110,7 +110,7 @@ public class remove extends sort {
             }
         }
 
-        for (int i = 0; i < remove.length; i++) {
+        for (int i = 0; i < remove.length; i++) {                         // display
             for (int j = 0; j < remove[i].length; j++) {
                 System.out.print(remove[i][j] + " ");
             }
