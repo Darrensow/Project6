841016134dca24a59958f2d99c77f782b464552b
Darrensow <Darrensow@hotmail.com>
2021-02-03 21:35:09 +0800

    Test run

--- DonaldDuck/src/Main/Main.java
+++ DonaldDuck/src/Main/Main.java
@@ -2,6 +2,9 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Test");
+    }
+    public static boolean test(){
+        return false;
     }
 }
