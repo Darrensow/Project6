fbe70c083121d0914faa9d25dc4174527959b6f7
PuaZhiXian <78425201+PuaZhiXian@users.noreply.github.com>
2021-02-03 23:32:38 +0800

    Update calculate.java

--- DonaldDuck/src/Main/calculate.java
+++ DonaldDuck/src/Main/calculate.java
@@ -2,22 +2,22 @@ package Main;
 
 public class calculate extends remove{
     public void find(){
-        boolean[] is_digit=new boolean[data[0].length];
+        boolean[] is_digit=new boolean[data[0].length];       // determine whether this colomun is digit or letter
         for (int i = 0; i < is_digit.length; i++) {
             is_digit[i]=true;
         }
         
-        for (int i = 1; i< data.length; i++) {
+        for (int i = 1; i< data.length; i++) {                      // checking method
             for (int j = 0; j < data[i].length; j++) {
                 for (int k = 0; k < data[i][j].length(); k++) {
-                    if(Character.isLetter(data[i][j].charAt(k))==true&& !data[i][j].equals(" ")){
+                    if(Character.isLetter(data[i][j].charAt(k))==true&& !data[i][j].equals(" ")){     
                         is_digit[j] = false;
                         break;
                     }
                 } 
             }
         }
-        for (int i = 0; i < is_digit.length; i++) {
+        for (int i = 0; i < is_digit.length; i++) {       // display boolean array
             System.out.println(is_digit[i]);
         }
     }
