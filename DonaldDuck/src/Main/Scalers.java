16ccb3f5ed7ba8204329dc9a9453de7afb0c230e
thaiming0123 <75868791+thaiming0123@users.noreply.github.com>
2021-02-07 02:42:55 +0800

    Update Scalers.java

--- DonaldDuck/src/Main/Scalers.java
+++ DonaldDuck/src/Main/Scalers.java
@@ -9,21 +9,55 @@ public class Scalers extends StatisticsPackage{
         this.minmaxscale = minmaxscale;
     }
 
-    //subtarct the mean from all values in the column and divide by the standard deviation
-    public double [] StandardScaling(){
+    //display the output for standardscaling
+    public void displayStandardScaling(){
         standardscale = new double [specificColumn.length];
         for (int i = 0; i < specificColumn.length ; i++) {
-            standardscale[i] = (specificColumn[i] - getMean()) / getStandardDeviation() ;
+            standardscale[i] = StandardScaling(specificColumn[i]);
         }
-        return standardscale;
+
+        System.out.println("Standard Scaling : ");
+        for (int i = 0; i < standardscale.length; i++) {
+            if (i == 0) {
+                System.out.print("{" + standardscale[i] + ", ");
+            }
+            else if (i>0 && i < standardscale.length-1) {
+                System.out.print(standardscale[i] + ", ");
+            }
+            else{
+                System.out.print(standardscale[i] + "}");
+            }
+        }
+    }
+
+    //display the output of MinMaxScaling
+    public void displayMinMaxScaling(){
+        minmaxscale = new double[specificColumn.length];
+        for (int i = 0; i < specificColumn.length ; i++) {
+            minmaxscale[i] = MinMaxScaling(specificColumn[i]);
+        }
+
+        System.out.println("Min Max Scaling : ");
+        for (int i = 0; i < minmaxscale.length; i++) {
+            if (i == 0) {
+                System.out.print("{" + minmaxscale[i] + ", ");
+            }
+            else if (i>0 && i < minmaxscale.length-1) {
+                System.out.print(minmaxscale[i] + ", ");
+            }
+            else{
+                System.out.print(minmaxscale[i] + "}");
+            }
+        }
+    }
+
+    //subtarct the mean from all values in the column and divide by the standard deviation
+    public double StandardScaling(double Specific){
+         return (Specific - getMean()) / getStandardDeviation() ;
     }
 
     //subtracting the min from all values in the column and dividing by the range
-    public double[] MinMaxScaling(){
-        minmaxscale = new double[specificColumn.length];
-        for (int i = 0; i < specificColumn.length; i++) {
-            minmaxscale[i] = (specificColumn[i] - getMinValue()) / getRange();
-        }
-        return minmaxscale;
+    public double MinMaxScaling(double specific){
+        return (specific - getMinValue()) / getRange();
     }
 }
